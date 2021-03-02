b8b4f91fe94a8881a614c4a7aa15ca3b9f5de2dc
Sahal Shaikh <shaikhsahal125@gmail.com>
2021-03-02 12:44:25 -0500

    will continue work on getting equation of time

--- calculatePrayerTimes.py
+++ calculatePrayerTimes.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
-print("This will calculate time")
+import pandas as pd
+
+
+class PrayerTimeCalculations:
+
+	def __init__(self):
+		dateAndTime = pd.datetime.now()
+		self.year = dateAndTime.year
+		self.month = dateAndTime.month
+		self.day = dateAndTime.day
+		print(dateAndTime)
+
+	def get_julian_date(self):
+		date = pd.Timestamp(year=self.year, month=self.month, day=self.day)
+		return date.to_julian_date()
+
+
+	def get_declination_and_EqT(self, julian_date):
+		pass
+
+
+if __name__ == "__main__":
+	pt = PrayerTimeCalculations()
+	print(pt.get_julian_date())
+
 
