30d3860f901850485fe275f05a7378bcff3a2342
Sahal Shaikh <shaikhsahal125@gmail.com>
2021-03-03 08:29:04 -0500

    azan added

--- calculatePrayerTimes.py
+++ calculatePrayerTimes.py
@@ -5,6 +5,7 @@ from math import atan2 as arctan2
 from math import asin as arcsin
 
 import pandas as pd
+import datetime
 
 
 class PrayerTimeCalculations:
@@ -22,6 +23,13 @@ class PrayerTimeCalculations:
 	def get_julian_date(self):
 		date = pd.Timestamp(year=self.year, month=self.month, day=self.day)
 		return date.to_julian_date()
+
+	def get_julian(self):
+		a = (14 - self.month) // 12
+		y = self.year + 4800 - a
+		m = self.month + 12*a - 3
+		jd = self.day + (153*m + 2)/5 + 365*y + y/4 - y/100 + y/400 - 32045
+		return int(jd)
 
 
 	def get_declination_and_EqT(self, julian_date):
@@ -42,17 +50,20 @@ class PrayerTimeCalculations:
 
 
 
-	
+
 
 if __name__ == "__main__":
 	pt = PrayerTimeCalculations(40.277310, -74.561890)
 	jd = pt.get_julian_date()
 	print(jd)
-	dAndEqT = pt.get_declination_and_EqT(jd)
-	print(dAndEqT)
 
-	dhuhar = 12 + (-5) - (((-74.561890)/15) - (dAndEqT[1]))
-	print(f'Dhuhar time is: {dhuhar}')
-
-	lol = None
-	print(f'Actual time: {lol}')
+	jd2 = pt.get_julian()
+	print(jd2)
+	# dAndEqT = pt.get_declination_and_EqT(jd)
+	# print(dAndEqT)
+	#
+	# dhuhar = 12 + (-5) - (((-74.561890)/15) - (dAndEqT[1]))
+	# print(f'Dhuhar time is: {dhuhar}')
+	#
+	# lol = None
+	# print(f'Actual time: {lol}')
---
+++ test.py
@@ -0,0 +1,19 @@
+from praytimes import PrayTimes
+from datetime import date
+import os
+
+pt = PrayTimes()
+dic = {'imsak':0, 'fajr':5, 'sunrise':0, 'dhuhr':0, 'asr':0, 'maghrib':-16, 'isha':4, 'midnight':0}
+
+dt = date(2021, 4, 3)
+cords = (40.277310, -74.561890)
+pt.tune(dic)
+
+times = pt.getTimes(dt, cords, -5, dst=1)
+
+for k, v in times.items():
+    print(f'{k} : {v}')
+
+print("playing..")
+os.system('mpg321 ./azans/fazar/FajrAzan1.mp3')
+print("Done!")
